8d5886da23704464eb6df5a9d137978719a6fd42
Charlie Dalsass <test@test.com>
2014-12-30 18:34:15 -0500

    added location distanceTo function

--- src/test/java/LocatableListTest.java
+++ src/test/java/LocatableListTest.java
@@ -3,6 +3,7 @@
 
 import static org.junit.Assert.assertEquals;
 import com.iqtransit.server.LocatableList;
+import com.iqtransit.geo.Locatable;
 import org.junit.Test;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
@@ -16,19 +17,25 @@ import org.junit.runners.JUnit4;
 public class LocatableListTest {
 
     @Test
-    public void thisAlwaysPasses() {
+    public void testAddingAndDeduping() {
     	LocatableList a = new LocatableList();
     	String id = "user1000";
-    	double lat = 72.00;
-    	double longitude = 72.00;
-    	a.locationUpdate(id, lat, longitude);
-    	a.getLocations();
-    	org.junit.Assert.assertEquals("should be 1 item in last after 1 unique id used",1, a.size());
-    	a.locationUpdate("ANOTHER user", 73.00, longitude);
-    	org.junit.Assert.assertEquals("should be 2 items in list after 2 unique ids used", 2, a.size());
-    	a.locationUpdate(id, lat, longitude);
-    	org.junit.Assert.assertEquals("based on id number, dups should be eliminated", 2, a.size());
+        double lat = 72.00;
+        double longitude = 72.00;
+        a.locationUpdate(id, lat, longitude);
+        a.getLocations();
+        org.junit.Assert.assertEquals("size() should be 1 item in last after 1 unique id used",1, a.size());
+        a.locationUpdate("different user", 73.00, longitude);
+        org.junit.Assert.assertEquals("should be 2 items in list after 2 unique ids used", 2, a.size());
+        a.locationUpdate(id, lat, longitude);
+        org.junit.Assert.assertEquals("based on id number, dups should be eliminated", 2, a.size());
+    }
 
+    @Test
+    public void testLocationFunctions() {
+        Locatable from = new Locatable("from",42.520536, -71.507640);
+        Locatable to = new Locatable("to",42.501555, -71.476741);
+        org.junit.Assert.assertEquals("distance should be about 2 miles", true, from.distanceTo(to) > 1.5 &&  from.distanceTo(to) < 2.5);
     }
 
    
